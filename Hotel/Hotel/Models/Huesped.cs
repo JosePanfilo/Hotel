7f4d9abbadcc24e4484828f033926e5c6c84cd08
LeobardoMorales <leo_96@hotmail.es>
2016-01-20 19:21:15 -0700

    Cree el modelo de huesped

---
+++ Hotel/Hotel/Models/Huesped.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.Models
+{
+    public class Huesped
+    {
+        //Se crea modelo de huesped
+        public class Huesped
+        {
+            public int huespedID { get; set; }
+            public String nombre { get; set; }
+            public String apellidoP { get; set; }
+            public String apellidoM { get; set; }
+            public String telefono { get; set; }
+
+            // Huesped tiene una coleccion de reservaciones
+            //   public ICollection<Reservacion> reservaciones { get; set; }
+        }
+    }
+}
